a80c829a1bf7963b8cfd754107440ed08460f7bd
Emerson Rodrigues <emersonrodrigues5578@gmail.com>
2023-12-20 16:29:40 -0300

    Atualização de usuário com PUT

--- src/main/java/com/tecnicoemerson/workshopmongo/resources/UserResource.java
+++ src/main/java/com/tecnicoemerson/workshopmongo/resources/UserResource.java
@@ -41,10 +41,20 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@PutMapping("/{id}")
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	
 	
 }
