bc967e4a950a9b0643790a91552e15e65e9da059
Emerson Rodrigues <emersonrodrigues5578@gmail.com>
2023-12-21 00:35:57 -0300

    Consulta com vários critérios

--- src/main/java/com/tecnicoemerson/workshopmongo/resources/PostResource.java
+++ src/main/java/com/tecnicoemerson/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.tecnicoemerson.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,8 @@ import com.tecnicoemerson.workshopmongo.services.PostService;
 @RestController
 @RequestMapping(value="/posts")
 public class PostResource {
-
 	@Autowired
 	private PostService service;
-
 	@GetMapping("/{id}")
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
@@ -28,9 +27,21 @@ public class PostResource {
 	}
 	
 	@GetMapping("/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping("/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/tecnicoemerson/workshopmongo/services/PostService.java
+++ src/main/java/com/tecnicoemerson/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.tecnicoemerson.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,8 +20,13 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
